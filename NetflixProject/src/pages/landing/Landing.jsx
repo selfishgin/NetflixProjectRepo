a9a10ec4a2ed63519bc6fe60895ed3283fdf6d40
Huseynli Ahmad Ramiz <Husey_ij91@itstep.edu.az>
2024-12-16 12:04:02 +0400

    Inclass

---
+++ NetflixProject/src/pages/landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const Landing = () => {
+  return (
+    <>
+
+        <Navbar/>
+        
+    </>
+  )
+}
+
+export default Landing
---
+++ NetflixProject/src/pages/landing/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <div className='bg-gradient-to-b from-black to-transparent h-[120px] w-full justify-between items-center flex flex-row'>
+
+            <div className='w-full mx-48 justify-between items-center flex flex-row'>
+                <div className='w-fit'><img src="src/assets/NetflixLogo.png" alt="" /></div>
+                <div className=' justify-between flex gap-2'>
+                    <select id="lang" className='w-[7rem] bg-black text-white border rounded border-white-100'>
+                        <option value="english">English</option>
+                        <option value="azerbaijani">Azerbaijani</option>
+                    </select>
+
+                    <button className='bg-red-600 rounded p-1 w-[5rem] text-white'>Sign in</button>
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
